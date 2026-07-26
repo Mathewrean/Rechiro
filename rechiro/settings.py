5abaeed61f02c77c6a03bc32eb2287ec5a1aa325
Mathewrean <mathewrean@gmail.com>
2026-07-26 03:29:51 +0300

    fix(email): add EMAIL_USE_SSL support for port 465 Gmail

--- rechiro/settings.py
+++ rechiro/settings.py
@@ -239,6 +239,7 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@rechiro.com')
 EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_TIMEOUT = 10
